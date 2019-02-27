dadb04808f8cf6f3115b5a3e09079b66eb0247d0
dodose <saiei2484@gmail.com>
2019-02-27 10:44:40 +0900

    GameMgr : 아이템 이미지 이름 변경, HpBar : HP최대 한계치 value값 지정

--- Jaeyoung/HpBar.cs
+++ Jaeyoung/HpBar.cs
@@ -5,57 +5,24 @@ using UnityEngine.UI;
 
 public class HpBar : MonoBehaviour {
     Slider slider;
-    GameObject enemyUI;
-    Vector3 sliderPos;
-    public float distance = 5;
+    int IsInit = (int)eBOOLEAN.FALSE; // 초기화를 했는가? 
 
     private void Awake()
     {
-        enemyUI = GameObject.Find("enemyUI");
-        slider = enemyUI.transform.GetChild(0).GetComponent<Slider>();
+        slider = transform.GetComponent<Slider>();
     }
 
-    private void Start()
+    public void changeHpBar(int value) 
     {
-        slider.minValue = 0;
-        /* player, enemy가 가진 weapon에 따라 hp 값 초기화 */
-        if(slider.name == "enemyHP")
+        if(IsInit == (int)eBOOLEAN.FALSE) 
         {
-            slider.maxValue = 10; //player, enemy weapon 불러오기 
+            IsInit = (int)eBOOLEAN.TRUE;
+            slider.maxValue = value;
+            slider.value = value;
+        }
+        if (slider.maxValue >= value)
+            slider.value = value;
+        else
             slider.value = slider.maxValue;
-
-        }
-        else if(slider.name == "playerHP")
-        {
-            slider.maxValue = 10;
-            slider.value = slider.maxValue;
-        }
-
-    }
-
-    private void FixedUpdate()
-    {
-        /* slider의 부모(canvas) 움직임 제어 */
-        sliderPos = GameObject.Find("Enemy").transform.position;
-        sliderPos.y += distance;
-        enemyUI.transform.position = sliderPos;
-    }
-
-    void changeHpBar(int changeNum , int effect) //damage or heal 구분 
-    {
-        int IsValueChangeOK = 0;
-        if (effect == (int)eHPEFEECT.em_DAMAGE)
-        {
-            IsValueChangeOK = (int)slider.value - changeNum;
-            if (IsValueChangeOK >= slider.minValue)
-                slider.value = IsValueChangeOK;
-        }
-            
-        else if (effect == (int)eHPEFEECT.em_HEAL)
-        {
-            IsValueChangeOK = (int)slider.value + changeNum;
-            if (IsValueChangeOK <= slider.maxValue)
-                slider.value = IsValueChangeOK;
-        }
     }
 }
